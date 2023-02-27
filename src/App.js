a63a128593c33dc66c453e6ebbb0001a403a13f8
mtettmar <Marcus.Tettmar@mjtnet.com>
2023-02-27 18:16:22 +0000

    tweaks

--- src/App.js
+++ src/App.js
@@ -16,17 +16,16 @@ function App() {
 
   const [accessCode, setAccessCode] = useState('');
   const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [nino, setNino] = useState('');
-  const [dob, setDob] = useState('');
+  const [importData, setImportData] = useState('');
+  const [arrayData, setArrayData] = useState([]);
   const [done, setDone] = useState(false);
   const [error, setError] = useState("");
 
   const saveDetails = () => {
-    console.log(email, name, nino, dob);
+    console.log(email, importData);
     setDone(false);
     //post data to https://api.untied.io/tin-verification/save.php
-    fetch(`https://${api_host}/tin-verification/save.php`, {
+    fetch(`https://${api_host}/irv-registration/save.php`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -34,9 +33,7 @@ function App() {
         body: JSON.stringify({
           accessCode: accessCode,
           email: email,
-          name: name,
-          nino: nino,
-          dob: dob
+          data: importData
           })
         })
         .then(response => response.json())
@@ -44,12 +41,10 @@ function App() {
           if (data.status === 'success') {
             
             setAccessCode('');
-            setEmail('');
-            setName('');
-            setNino('');
-            setDob('');
+            setImportData('');
             setError('');
             setDone(true);
+            getRecords()
           } else {
             setError(data.message);
           }
@@ -59,7 +54,46 @@ function App() {
         console.error('Error:', error);
 
       }
-    );
+   );
+
+  }
+
+
+  const getRecords = () => {
+    console.log(email, importData);
+    setDone(false);
+    //post data to https://api.untied.io/tin-verification/save.php
+    fetch(`https://${api_host}/irv-registration/list.php`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          accessCode: accessCode,
+          })
+        })
+        .then(response => response.json())
+        .then(data => {
+          if (data.status === 'success') {
+
+            const adata = data.data
+            //sort adata by date_processed attribute
+            adata.sort(function(a, b) {
+              return new Date(b.date_processed) - new Date(a.date_processed);
+            });
+            
+            setArrayData(adata);
+            console.log(data)
+          } else {
+            setError(data.message);
+          }
+        }
+      )
+      .catch((error) => {
+        console.error('Error:', error);
+
+      }
+   );
 
   }
 
@@ -69,17 +103,22 @@ function App() {
       <Container fluid="sm" style={{ maxWidth: 600 }}>
 
         <Row className="mt-2">
-          <img src={require('./ulogo.svg').default} alt="untied logo"/>
+          <img src={require('./tigor_beta_logo.svg').default} alt="untied logo"/> <br />      
+        </Row>
+
+        <Row className="mb-2">
+
+        {/* <h1>Agent Registration Service</h1> */}
         </Row>
 
         <Row className="pt-4">        
 
-        {done && <Card className="mt-4 mb-4" bg="success" text="light"><Card.Body><Card.Title>Thank you</Card.Title><Card.Text>We have received your details and will send you an email with the results.</Card.Text></Card.Body></Card>}
+
+        {done && <Card className="mt-4 mb-4" bg="success" text="light"><Card.Body><Card.Title>Thank you</Card.Title><Card.Text>Your data has been imported and will be processed in the next few minutes.</Card.Text></Card.Body></Card>}
 
         {error!=="" && <Card className="mt-4 mb-4" bg="danger" text="light"><Card.Body><Card.Title>Error</Card.Title><Card.Text>{error}</Card.Text></Card.Body></Card>}
 
-          <p>Check a seller's name, national insurance number and date of birth against HMRC records.</p>
-
+         
         </Row>
 
         <Row>
@@ -96,7 +135,7 @@ function App() {
 
             </Form.Group>
 
-
+{/* 
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Your email address</Form.Label>
               <Form.Control type="email" placeholder="Enter your email" value={email}
@@ -107,42 +146,23 @@ function App() {
               <Form.Text className="text-muted">
                 This is where we will send the verification results. Your email won't be used for any other purpose.
               </Form.Text>
-            </Form.Group>
+            </Form.Group> */}
 
             <hr />
-            <h3>Details of the subject (seller) to be checked</h3>
+            <h3>Import</h3>
             
 
             <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Full name</Form.Label>
-              <Form.Control type="text" placeholder="Enter full name" value={name}
+              <Form.Label>Import client records</Form.Label>
+              <Form.Control as="textarea" rows={6} type="text" placeholder="NX112233A,1980-11-15,CLIENT@EMAIL.XYZ" value={importData}
                 onChange={(e) => {
-                  setName(e.target.value);
+                  setImportData(e.target.value);
                 }}
               />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>National Insurance Number (NINO)</Form.Label>
-              <Form.Control type="text" placeholder="Enter NINO" value={nino}
-                onChange={(e) => {
-                  setNino(e.target.value);
-                }}
-              />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Date of birth</Form.Label>
-              <Form.Control type="date" placeholder="Enter DoB" value={dob}
-                onChange={(e) => {
-                  setDob(e.target.value);
-                }}
-              />
-
             </Form.Group>
 
             <p>
-              By pressing submit you confirm the subject has agreed to this check and that you accept the <a href="https://www.untied.io/tvs_terms" target="_new">terms and conditions</a>.
+              By pressing submit you confirm that you accept the <a href="https://www.untied.io/tvs_terms" target="_new">terms and conditions</a>.
             </p>
 
             <Button variant="primary" type="submit"
@@ -153,13 +173,53 @@ function App() {
             >
               Submit
             </Button>
+
+            <Button variant="primary" type="submit"
+              onClick={(e) => {
+                e.preventDefault();
+                getRecords();
+              }}
+              className="ms-2"
+            >
+              Just view records
+            </Button>
+
           </Form>
 
         </Row>
 
-        <Row>
+        {/* <Row>
           <p className="mt-2">Checks take 2 or 3 minutes. You will receive an email with the results, indicating whether the NINO and DoB match and a percentage match for the name.</p>
-        </Row>
+        </Row> */}
+        
+        {arrayData.length>0 && <Row className="mt-4">
+          <h3>Records</h3>
+          <table className="table table-striped" style={{fontSize:12}}>
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">NINO</th>
+                <th scope="col">DoB</th>
+                <th scope="col">Email</th>
+                <th scope="col">Processed</th>
+                <th scope="col">Date</th>
+                </tr>
+                </thead>
+                <tbody>
+                  {arrayData.map((item, index) => (
+                    <tr key={index}>
+                      <th scope="row">{item.id}</th>
+                      <td>{item.nino}</td>
+                      <td>{item.dob}</td>
+                      <td>{item.email}</td>
+                      <td>{item.processed}</td>
+                      <td>{item.date_processed}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+        </Row>}
+        
 
       </Container>
 
